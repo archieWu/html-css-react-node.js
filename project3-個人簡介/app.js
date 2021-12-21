b8769290018dc5305397f944ea9a255b9dd68e28
吳政樺 <yij1471@gmail.com>
2021-12-21 11:08:18 +0800

    動畫修改

--- project3-個人簡介/app.js
+++ project3-個人簡介/app.js
@@ -1,5 +1,12 @@
 const text = document.querySelectorAll('.thePaths');
  
 for(let i = 0; i< text.length; i++){
-     console.log(text[i])
+     console.log(text[i].getTotalLength())
 }
+
+const lestword = document.querySelector('#fifteen');
+
+const animation = document.querySelector('div.animation');
+lestword.addEventListener("animationend", () => {
+    animation.style="transition: all 1s ease; opacity: 0; pointer-events: none;";
+})
